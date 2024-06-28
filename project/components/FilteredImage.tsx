f9a727c378aa79c9ba8fd080944ce538d538cebd
SergioCarr <carreon.sergioivan@gmail.com>
2024-06-28 09:58:29 -0600

    se ve un charizard en la home page

--- project/components/FilteredImage.tsx
+++ project/components/FilteredImage.tsx
@@ -4,28 +4,28 @@ import { AdvancedImage } from 'cloudinary-react-native';
 import { Cloudinary } from "@cloudinary/url-gen";
 
 // Import required actions.
-import { sepia } from "@cloudinary/url-gen/actions/effect";
+import { cartoonify } from "@cloudinary/url-gen/actions/effect";
 
 export default function FilteredImage() {
 
     // Create and configure your Cloudinary instance.
     const cld = new Cloudinary({
         cloud: {
-            cloudName: 'dpjwt3wc0'
+            cloudName: 'dttc4c0s5'
         }
     });
 
     // Use the image with public ID, 'front_face'.
-    const myImage = cld.image('cld-sample-2');
+    const myImage = cld.image('5020913e-18b8-4aed-914a-be5c62d7c528');
 
     // Apply the transformation.
     myImage
-      .effect(sepia());  // Apply a sepia effect.
+      .effect(cartoonify());  
 
     // Render the transformed image in a React Native component.
     return (
       <SafeAreaView style={{flex: 1, justifyContent: 'center'}}>
-        <AdvancedImage cldImg={myImage} style={{ width: 400, height: 250, alignSelf: 'center'}} />
+        <AdvancedImage cldImg={myImage} style={{ width: 150, height: 200, alignSelf: 'center'}} />
       </SafeAreaView>
     )
 };
